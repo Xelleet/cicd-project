0e7859bb87f62af16a6dce2000bbbad566c5ce8f
Xelleet <ererenyeager@yandex.ru>
2026-05-19 17:40:47 +0300

    nice calc

--- calculator.py
+++ calculator.py
@@ -1,14 +1,14 @@
 def add(a, b):
-    return a + b + 1
+    return a + b
 
 def subtract(a, b):
-    return a - b + 1
+    return a - b
 
 def multiply(a, b):
-    return a * b + 1
+    return a * b
 
 def divide(a, b):
-    return a / b + 1
+    return a / b
 
 if __name__ == "__main__":
     print("Калькулятор v1.0")
