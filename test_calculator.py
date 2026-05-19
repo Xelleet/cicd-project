d4fd9cab952617242048018cfab877ec0f5c14ae
Xelleet <ererenyeager@yandex.ru>
2026-05-19 17:37:49 +0300

    upd

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 5, "Ошибка в сложении"
+    assert add(2, 3) == 8, "Ошибка в сложении"
 
 def test_subtract():
     assert subtract(5, 2) == 3, "Ошибка в вычитании"
